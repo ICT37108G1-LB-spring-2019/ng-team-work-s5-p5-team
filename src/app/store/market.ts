56927defbc7537577628415bd907c96c68bf2146
shalva02 <zaqaidzes@gmail.com>
2019-07-23 11:52:38 +0400

    QUIZZE

---
+++ src/app/store/market.ts
@@ -0,0 +1,19 @@
+import { Product } from "./product.model";
+
+export const PRODUCTS: Product[] = [
+    {
+      id: 0,
+      name: "Nike Zoom Winflo 6",
+      price: 200
+    },
+    {
+      id: 1,
+      name: "Nike Air Zoom Pegasus 36",
+      price: 300
+    },
+    {
+      id: 2,
+      name: "ECCO Golf Biom Hybrid 3 GTX",
+      price: 400
+    }
+]
